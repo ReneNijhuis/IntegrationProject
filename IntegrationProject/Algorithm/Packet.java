d8b490cb08a76ba21f7aeed619c339bedcbc125a
flomatable <florianmat@live.nl>
2014-04-07 13:36:38 +0200

    packet en algoritme

---
+++ IntegrationProject/Algorithm/Packet.java
@@ -0,0 +1,43 @@
+package Algorithm;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+public class Packet {
+
+	private DatagramPacket datagram;
+	private InetAddress source;
+	private int TTL;
+	public Packet(DatagramPacket datagram){
+		datagram = this.datagram;
+	}
+
+	private byte[] data = datagram.getData();
+
+	public InetAddress getDestination(){
+		return datagram.getAddress();
+	}
+	public void setSource(InetAddress source){
+		source = this.source;
+	}
+	public InetAddress getSource(){
+		return source;
+	}
+	public void drop(){
+
+	}
+	public void setTTL(int TTL){
+		TTL = this.TTL;
+	}
+	public int getTTL(){
+		return TTL;
+	}
+	public void forward(){
+
+	}
+	public void accept(){
+
+	}
+
+}
+

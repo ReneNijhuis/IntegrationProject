ce3dc96e97855401ffd9246b4a7356b261d12cbc
Rob van Emous <robvemous@gmail.com>
2014-04-07 23:44:24 +0200

    Update
    -Removed old files
    -changed Main class

--- IntegrationProject/src/applicationLayer/Main.java
+++ IntegrationProject/src/applicationLayer/Main.java
@@ -1,5 +1,8 @@
 package applicationLayer;
 
+import transportLayer.PacketRouter;
+import connectionLayer.Client;
+
 /**
  * Should become the (executable) main of our whole
  * @author Rob van Emous
@@ -9,6 +12,13 @@ public class Main {
 	MainUI mainUI;
 	
 	public Main() {
+		// start UI
 		mainUI = new MainUI(this);
+		// start Ad-Hoc-client
+		Client client = new Client();
+		client.start();
+		// start packet-router
+		PacketRouter router = new PacketRouter(client);
+		router.start();		
 	}
 }

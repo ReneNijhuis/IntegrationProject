fe8d3d6f8adba7f2b6cd35b14d1a471cf4d0587b
René Nijhuis <renenijhuis@home.nl>
2014-04-08 15:55:43 +0200

    Added documentation to the ByteUtils class

--- IntegrationProject/src/tools/ByteUtils.java
+++ IntegrationProject/src/tools/ByteUtils.java
@@ -1,7 +1,17 @@
 package tools;
 
+/**
+ * All functions used for converting to and from byte arrays.
+ * @author René Nijhuis & Rob van Emous
+ * @version 0.1
+ */
+
 public class ByteUtils {
 
+	/**
+	 * returns the given short as an array of two bytes.
+	 * @param s the short to get the bytes form
+	 */
 	public static byte[] shortToBytes(short s) {
 		byte[] array = new byte[2];
 		array[0] = (byte) (s&0xF0 >>> 8);
@@ -9,16 +19,21 @@ public class ByteUtils {
 		return array;
 	}
 	
+	/**
+	 * converts the first two bytes of the array to a short.
+	 * @param bytes the byte array to convert
+	 * @return the short the array was converted to
+	 */
 	public static short bytesToShort(byte[] bytes) {
-		if (bytes.length > 2) {
-			//cannot convert to int
-			return 0;
-		}
 		short result = 0;
 		result = (short) ((bytes[0] << 8)| bytes[1]);		
 		return result;
 	}
 	
+	/**
+	 * returns the given short as an array of two bytes. 
+	 * @param i the integer to get the bytes form
+	 */
 	public static byte[] intToBytes(int i) {
 		byte[] result = new byte[4];
 		result[0] = (byte)(i&0xF000 >>> 24);
@@ -28,11 +43,12 @@ public class ByteUtils {
 		return result;
 	}
 	
+	/**
+	 * converts the first four bytes of the array to an integer.
+	 * @param bytes the byte array to convert
+	 * @return the integer the array was converted to
+	 */
 	public static int bytesToInt(byte[] bytes) {
-		if (bytes.length > 4) {
-			// cannot convert to int
-			return 0;
-		}
 		int result = 0;
 		result = (int) ((bytes[0]<<24)| (bytes[1]<<16) | (bytes[2]<<8) | bytes[3]);
 		return result;

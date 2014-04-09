57f169b38fa24d9f8720bb691dc6084ac5ee38ec
René Nijhuis <renenijhuis@home.nl>
2014-04-09 10:50:24 +0200

    Made a universal testing tool, encryption test to be finished by Wim

--- IntegrationProject/src/tools/TestingTool.java
+++ IntegrationProject/src/tools/TestingTool.java
@@ -1,17 +1,26 @@
 package tools;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TestByteUtils {
+import encryptionLayer.Encryption;
+
+public class TestingTool {
 	
 	private static Random rand;
 	private short[] shorts;
 	private int[] ints;
+	private byte[][] text;
+	private Encryption encrypt;
 	
 	public static void main(String[] arg) {
 		rand = new Random(new Random().nextLong());
-		new TestByteUtils().runTest();
+		new TestingTool().runTest();
+	}
+	
+	public TestingTool() {
+		encrypt = new Encryption();
 	}
 
 	private void runTest() {
@@ -38,8 +47,15 @@ public class TestByteUtils {
 							output("ERROR: Need a number not a word");
 						}
 						break;
+					case "text":
+						try {
+							generateEncrText(Integer.parseInt(input.next()), Integer.parseInt(input.next()));
+						} catch (java.lang.NumberFormatException e) {
+							output("ERROR: Need a number not a word");
+						}
+						break;
 					default:
-						output("what? int or short?");
+						output("what? int, text or short?");
 						break;
 				}
 				break;
@@ -56,8 +72,11 @@ public class TestByteUtils {
 					case "int":
 						testInts();
 						break;
+					case "text":
+						testText();
+						break;
 					default:
-						output("what? int or short?");
+						output("what? int, text or short?");
 						break;
 				}
 				break;
@@ -71,7 +90,7 @@ public class TestByteUtils {
 		output("Test stopped");
 		input.close();
 	}
-	
+
 	private void show(String toShow) {
 		switch(toShow) {
 			case "short":
@@ -98,26 +117,46 @@ public class TestByteUtils {
 					output("ERROR: No ints generated. Use 'generate int <amount>' first.");
 				}
 				break;
+			case "text":
+				if (text != null) {
+					for (byte[] ba : text) {
+						output(textArrayToString(ba));
+					}
+				} else {
+					output("ERROR: No texts generated. " +
+							"Use 'generate text <amountOfTexts> <charactersPerText>' first.");
+				}
+				break;
 			default:
-				output("what? int or short?");
+				output("what? int, text or short?");
 				break;
 		}
 	}
 	
+	private void generateEncrText(int amountOfTexts, int charactersPerText) {
+		text = new byte[amountOfTexts][charactersPerText];
+		for (int i = 0; i < amountOfTexts; i++) {
+			for (int j = 0; j < charactersPerText; j++) {
+				text[i][j] = (byte) (32 + rand.nextInt(127-32)); //ascii characters range from 32 (space) to 126 (~)
+			}
+		}
+		output(amountOfTexts + " texts of " + charactersPerText + " characters to encrypt generated");
+	}
+	
 	private void generateShorts(int amount){
-		output(amount + " shorts generated");
 		shorts = new short[amount];
 		for (int i = 0; i < amount; i++) {
 			shorts[i] = (short) rand.nextInt();
 		}
+		output(amount + " shorts generated");
 	}
 	
 	private void generateInts(int amount) {
-		output(amount + " ints generated");
 		ints = new int[amount];
 		for (int i = 0; i < amount; i++) {
 			ints[i] = rand.nextInt();
 		}
+		output(amount + " ints generated");
 	}
 
 	private void testInts() {
@@ -154,6 +193,25 @@ public class TestByteUtils {
 		}
 	}
 	
+	private void testText() {
+		if (text == null) {
+			output("ERROR: No texts generated. " + 
+					"Use 'generate text <amountOfTexts> <charactersPerText> first.");
+			return;
+		}
+		boolean errors = false;
+		for (int i = 1; i < text.length; i++) {
+			byte[] ba = text[i-1];
+			if (getResults(ba, i)) {
+				errors = true;
+			}
+		}
+		if (!errors) {
+			output("All tests successful");
+		}
+		
+	}
+	
 	private boolean getResults(short s, int nr) {
 		byte[] sBytes = ByteUtils.shortToBytes(s);
 		short sRebuilt = ByteUtils.bytesToShort(sBytes);
@@ -183,6 +241,28 @@ public class TestByteUtils {
 		} else {
 			return false;
 		}
+	}
+	
+	private boolean getResults(byte[] ba, int nr) {
+		byte[] encText = null;	//TODO door Wim de encryptie van 'ba'
+		byte[] rebuiltText = null; //TODO door Wim de decryptie van 'encText'
+		if (Arrays.equals(rebuiltText, ba)) {
+			output("-------------------------------------------------");
+			output("Original text" + nr + ": " + textArrayToString(ba));		
+			output("Encryption of text" + nr + ": " + textArrayToString(encText));
+			output("Rebuilt text" + nr + ": " + textArrayToString(rebuiltText));
+			output("-------------------------------------------------");
+		}
+		return false;
+	}
+	
+	private String textArrayToString(byte[] ba) {
+		String result = "{";
+		for (byte b : ba) {
+			result += (char) b;
+		}
+		result += "}";
+		return result;
 	}
 	
 	private void output(Object arg) {

c8c7aa1d457408928681e24507ea6ccca1805aef
flomatable <florianmat@live.nl>
2014-04-14 11:40:40 +0200

    routingprotocol met comments, speciaal voor rob <3 nog niet de routing table doorgevingzzz

--- IntegrationProject/src/transportLayer/RoutingProtocol.java
+++ IntegrationProject/src/transportLayer/RoutingProtocol.java
@@ -16,45 +16,53 @@ import connectionLayer.InternetProtocol;
 import applicationLayer.Main;
 
 public class RoutingProtocol implements Observer, Runnable {
-	
+
 	public static final int HEARTBEAT_INTERVAL = 100; //ms
-	
-	private Map<String, Integer> hopsPerNode = new HashMap<String, Integer>();
-	private Map<String, Long> directLinks = new HashMap<String, Long>();
-	private List<String> inetaddresses = new ArrayList<String>();
-	private byte[] HBData;
-	private byte[] MapData;
-	private Map<Integer, Integer> receivedHopsPerNode = new HashMap<Integer, Integer>();
-	private byte[] receivedData;
-	private boolean updatereceived = false;
-	
+
+	private Map<String, Integer> hopsPerNode = new HashMap<String, Integer>(); //routing table
+	private Map<String, Long> directLinks = new HashMap<String, Long>(); //map with 1 hops links
+	private List<String> inetaddresses = new ArrayList<String>(); //all registered addresses
+	private byte[] HBData; //data to be sent in heartbeat packets
+	private byte[] MapData; //routing table as byte[] to be sent
+	private Map<Integer, Integer> receivedHopsPerNode = new HashMap<Integer, Integer>();//received routing table
+	private byte[] receivedData; //received table as byte[]
+	private boolean updatereceived = false;//used for knowing whether routing table has to be updated
+
 	private Main main;
 	private PacketRouter router;
-	
+
 	private boolean stop = false;
-	
+
 	public RoutingProtocol(Main main, PacketRouter router) {
 		this.main = main;
 		this.router = router;
 	}
-
+	/**
+	 * fills the HBData with the string as byte[] used for heartbeats
+	 */
 	public void fillHB() {
 		String string = "Kaviaar";
 		HBData = string.getBytes();
 	}
-
+	/**
+	 * send a hearbeat packet
+	 */
 	public void heartBeat() {
 		fillHB();
 		router.sendPacket(Packet.generatePacket(HBData));
 	}
-
+	/**
+	 * send a packet with routing table as byte[]
+	 */
 	public void SendMap(){
 		fillHopsPerNode();
 		updateHopsPerNode();
 		fillMap();
 		router.sendPacket(Packet.generatePacket(MapData));
 	}
-
+	/**
+	 * updates the routing table according to the received routing table
+	 */
 	public void updateHopsPerNode(){
 		for(int i = 0; i < inetaddresses.size(); i ++){
 			if(i != InternetProtocol.MULTICAST_ADDRESS.getBytes()[3]){
@@ -66,7 +74,9 @@ public class RoutingProtocol implements Observer, Runnable {
 			}
 		}
 	}
-
+	/**
+	 * puts the directlinks in the routing table
+	 */
 	public void fillHopsPerNode(){
 		for(int i = 0; i < inetaddresses.size(); i ++){
 			if(directLinks.containsKey(inetaddresses.get(i))){
@@ -74,7 +84,9 @@ public class RoutingProtocol implements Observer, Runnable {
 			}
 		}
 	}
-
+	/**
+	 * fills the byte[] MapData with the routing table along with three-byte identifier as first three bytes
+	 */
 	public void fillMap(){
 		byte[] MapData2 = 	hopsPerNode.toString().getBytes();
 		MapData = new byte[MapData2.length + 3];
@@ -85,7 +97,9 @@ public class RoutingProtocol implements Observer, Runnable {
 		MapData[1] = 0x34;
 		MapData[2] = 0x12;
 	}
-
+	/**
+	 * removes a directlink from the table when the direct link times out
+	 */
 	public void deleteHost(){
 		for (int i = 0; i < inetaddresses.size(); i++){
 			long time = System.currentTimeMillis();
@@ -97,7 +111,10 @@ public class RoutingProtocol implements Observer, Runnable {
 			}
 		}
 	}
-
+	/**
+	 * receives packets, updates directlinks when a heartbeat is received if necessary and resets time out timer
+	 * fills receivedHopsPerNode if MapData received, noted by identifiers
+	 */
 	public void update(Observable observable, Object object) {
 		if (observable instanceof PacketRouter && object instanceof Packet) {
 			Packet packet = (Packet)object;
@@ -126,7 +143,11 @@ public class RoutingProtocol implements Observer, Runnable {
 			}
 		}
 	}
-
+	/**
+	 * converts a map to a byte[]
+	 * @param map
+	 * @return byte[]
+	 */
 	public static byte[] mapToByte(Map<Integer,Integer> map){
 		List<Integer> keys = new ArrayList<Integer>(map.keySet());
 		byte[] keysarray = new byte[2 * (keys.size())];
@@ -138,7 +159,11 @@ public class RoutingProtocol implements Observer, Runnable {
 
 		return keysarray;
 	}
-
+	/**
+	 * converts byte[] to a map
+	 * @param bytes
+	 * @return map
+	 */
 
 	public static Map<Integer,Integer> byteToMap(byte[] bytes){
 		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
@@ -165,7 +190,7 @@ public class RoutingProtocol implements Observer, Runnable {
 			} catch (InterruptedException e) {}
 		}
 	}
-	
+
 	/**
 	 * Shuts down routing protocol and optionally all lower layers.
 	 */
